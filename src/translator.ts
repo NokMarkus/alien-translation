1b5767109ef4164758f5d2f750d11aaaf4f68507
MarkusSchool <markus.nokelainen@tutors.es>
2024-12-19 14:57:23 +0200

    map upper/lowercase chars, nums to beep boop

--- src/translator.ts
+++ src/translator.ts
@@ -16,3 +16,18 @@ const numberSymbols = [
 
 const englishToAlien: Record<string, string> = {};
 const alienToEnglish: Record<string, string> = {};
+
+'abcdefghijklmnopqrstuvwxyz'.split('').forEach((char, index) => {
+    englishToAlien[char] = lowercaseSymbols[index];
+    alienToEnglish[lowercaseSymbols[index]] = char;
+});
+
+'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').forEach((char, index) => {
+    englishToAlien[char] = uppercaseSymbols[index];
+    alienToEnglish[uppercaseSymbols[index]] = char;
+});
+
+'0123456789'.split('').forEach((char, index) => {
+    englishToAlien[char] = numberSymbols[index];
+    alienToEnglish[numberSymbols[index]] = char;
+});
